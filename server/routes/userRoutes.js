a9cf0c590d6415753cc4c1aed7d7dd354f94f971
Hugo <78080239+HugoBiegas@users.noreply.github.com>
2023-05-18 10:42:39 +0200

    gestions de l'inscriptions est des erreur classique

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,6 +4,7 @@ const connection = require('../dbConfig');
 const path = require('path');  
 const fs = require('fs');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
@@ -51,6 +52,18 @@ router.get('/api/capchat/:urlUsage', (req, res) => {
     });
 });
 
+router.get('/api/urlusage', (req, res) => {
+    connection.query(`SELECT URLUsage FROM ImageSets`, function (error, results, fields) {
+        if (error) {
+            return res.status(500).send(error);
+        }
+        // Extraire l'URLUsage de chaque ligne de résultats
+        const urlUsages = results.map(result => result.URLUsage);
+        // Répondre avec les URLUsage en JSON
+        res.json(urlUsages);
+    });
+});
+
 
 
 router.get('*/capchat/:urlUsage', (req, res) => {
@@ -70,13 +83,74 @@ router.get('*/capchat/:urlUsage', (req, res) => {
 
 
   
-router.post('/inscription', (req, res) => {
-  // Code de gestion de l'inscription
+router.post('/inscription', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const username = req.body.username;
+        const NameArtiste = req.body.NameArtiste;
+        let query;
+        let values;
+
+        if (NameArtiste) {
+            query = 'SELECT * FROM Users WHERE Username = ? OR NameArtiste = ?';
+            values = [username, NameArtiste];
+        } else {
+            query = 'SELECT * FROM Users WHERE Username = ?';
+            values = [username];
+        }
+
+        connection.query(query, values, function (error, results, fields) {
+            if (error) {
+                console.error(error);
+                res.json({message: 'Erreur interne'});
+            } else if (results.length > 0) {
+                let message = results[0].Username === username ? "Nom d'utilisateur déjà pris" : "Nom d'artiste déjà pris";
+                res.json({message: message});
+            } else {
+                connection.query('INSERT INTO Users (Username, Password, NameArtiste) VALUES (?, ?, ?)', [username, hashedPassword, NameArtiste], function (error, results, fields) {
+                    if (error) {
+                        console.error(error);
+                        res.json({message: 'Erreur interne'});
+                    } else {
+                        res.json({message: 'Inscription réussie'});
+                    }
+                });
+            }
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.json({message: 'Erreur interne'});
+    }
 });
 
+
+
+
 router.post('/connexion', (req, res) => {
-  // Code de gestion de la connexion
+    const username = req.body.username;
+    const password = req.body.password;
+
+    connection.query('SELECT Password FROM Users WHERE Username = ?', [username], async function (error, results, fields) {
+        if (error) {
+            console.error(error);
+            res.redirect('/connexion');
+        } else {
+            if (results.length > 0) {
+                const comparison = await bcrypt.compare(password, results[0].Password)
+                if (comparison) {
+                    // l'utilisateur est connecté avec succès
+                    res.redirect('/welcome'); // ou toute autre route
+                } else {
+                    res.redirect('/connexion');
+                }
+            } else {
+                res.redirect('/connexion');
+            }
+        }
+    });
 });
+
 
 router.get('*', (req, res) => {
     res.redirect('/');
