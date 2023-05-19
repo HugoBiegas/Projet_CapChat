c96bb93a82d9e103651ce08a4e5052c440755332
Hugo <78080239+HugoBiegas@users.noreply.github.com>
2023-05-19 10:42:19 +0200

    mise en place des message d'erreur personaliser

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -36,6 +36,7 @@ router.use(cors());
 router.use('/views/image/neutres', express.static(path.join(__dirname, '..', 'views', 'image', 'neutres')));
 router.use('/views/image/singuliers', express.static(path.join(__dirname, '..', 'views', 'image', 'singuliers')));
 router.use('/views/image', express.static(path.join(__dirname, '..', 'views', 'image')));
+router.use('/views/erreur', express.static(path.join(__dirname, '..', 'views', 'erreur')));
 
 
 router.get('/', (req, res) => {
@@ -54,20 +55,20 @@ router.get('/api/capchat/:urlUsage', (req, res) => {
     const urlUsage = req.params.urlUsage;
     connection.query(`SELECT * FROM ImageSets WHERE URLUsage = ?`, [urlUsage], function (error, results, fields) {
         if (error) {
-            return res.status(500).send(error);
+          return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('error Base de Donnée non accésible')}`);
         }
         if (results.length === 0) {
-            return res.status(404).send('CapChat non trouvé');
+            return res.redirect(req.originalUrl + `/erreur/404?message=${encodeURIComponent('CapChat non trouvé')}`);
         }
         const capchat = results[0];
         connection.query(`SELECT FilePath FROM Images WHERE ImageSetID = ? AND Question IS NULL ORDER BY RAND() LIMIT 7`, [capchat.ID], function (error, results, fields) {
             if (error) {
-                return res.status(500).send(error);
+              return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('error Base de Donnée non accésible')}`);
             }
             const imagesNeutres = results;
             connection.query(`SELECT FilePath, Question FROM Images WHERE ImageSetID = ? AND Question IS NOT NULL ORDER BY RAND() LIMIT 1`, [capchat.ID], function (error, results, fields) {
                 if (error) {
-                    return res.status(500).send(error);
+                  return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('error Base de Donnée non accésible')}`);
                 }
                 const imageSinguliere = results;
                 res.json({imagesNeutres, imageSinguliere});
@@ -79,7 +80,7 @@ router.get('/api/capchat/:urlUsage', (req, res) => {
 router.get('/api/urlusage', (req, res) => {
     connection.query(`SELECT URLUsage FROM ImageSets`, function (error, results, fields) {
         if (error) {
-            return res.status(500).send(error);
+          return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('error Base de Donnée non accésible')}`);
         }
         // Extraire l'URLUsage de chaque ligne de résultats
         const urlUsages = results.map(result => result.URLUsage);
@@ -94,15 +95,16 @@ router.get('*/capchat/:urlUsage', (req, res) => {
     const urlUsage = req.params.urlUsage;
     connection.query(`SELECT id FROM ImageSets WHERE URLUsage = ?`, [urlUsage], function (error, results, fields) {
         if (error) {
-            return res.status(500).send(error);
+          return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('error Base de Donnée non accésible')}`);
         }
         if (results.length === 0) {
-            return res.status(404).send('CapChat non trouvé');
+          return res.redirect(req.originalUrl + `/erreur/404?message=${encodeURIComponent('CapChat non trouvé')}`);
         }
         // Si l'URLUsage existe, redirigez vers le fichier HTML
         res.sendFile(path.join(__dirname, '../views', 'capchat.html'));
     });
 });
+
 router.get('*/modification/:urlUsage', authenticateToken, async (req, res) => {
   try {
       const { urlUsage } = req.params;
@@ -110,20 +112,20 @@ router.get('*/modification/:urlUsage', authenticateToken, async (req, res) => {
       // Obtenir le capChat correspondant à l'URLUsage
       const capChats = await query('SELECT UserID FROM ImageSets WHERE URLUsage = ?', [urlUsage]);
       if (capChats.length === 0) {
-          return res.status(404).json({ message: "CapChat non trouvé" });
+        return res.redirect(req.originalUrl + `/erreur/404?message=${encodeURIComponent('CapChat non trouvé')}`);
       }
 
       const capChat = capChats[0];
 
       // Vérifier si l'utilisateur est le créateur de ce CapChat
       if (capChat.UserID !== req.user.id) {
-          return res.status(403).json({ message: "Accès refusé" });
+        return res.redirect(req.originalUrl + `/erreur/403?message=${encodeURIComponent('Accès refusé')}`);
       }
       res.sendFile(path.join(__dirname, '..', 'views', 'modificationsCapChat.html'));
   } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Erreur interne' });
-  }
+      return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('error Base de Donnée non accésible')}`);
+    }
 });
 
 
@@ -134,12 +136,12 @@ router.get('/api/capchatall/:urlUsage', authenticateToken, async (req, res) => {
       // Vérifier si le CapChat appartient à l'utilisateur
       const imageSet = await query('SELECT UserID FROM ImageSets WHERE URLUsage = ?', [urlUsage]);
       if (imageSet.length === 0 || imageSet[0].UserID !== req.user.id) {
-          return res.status(403).json({ message: "Vous ne pouvez pas resevoir ces informations" });
+        return res.redirect(req.originalUrl + `/erreur/403?message=${encodeURIComponent('Vous ne pouvez pas resevoir ces informations')}`);
       }
 
       const capchat = await query('SELECT ID FROM ImageSets WHERE URLUsage = ?', [urlUsage]);
       if (capchat.length === 0) {
-          return res.status(404).json({ message: "CapChat non trouvé" });
+        return res.redirect(req.originalUrl + `/erreur/404?message=${encodeURIComponent('CapChat non trouvé')}`);
       }
 
       const imagesNeutres = await query('SELECT FilePath FROM Images WHERE ImageSetID = ? AND Question IS NULL', [capchat[0].ID]);
@@ -148,7 +150,7 @@ router.get('/api/capchatall/:urlUsage', authenticateToken, async (req, res) => {
       res.json({ imagesNeutres, imageSinguliere });
   } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Erreur de serveur' });
+      return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('error Base de Donnée non accésible')}`);
   }
 });
 
@@ -157,12 +159,12 @@ router.get('/api/capchatall/:urlUsage', authenticateToken, async (req, res) => {
 router.post('/api/updateimage/:imagePath', authenticateToken, upload.single('imageFile'), async (req, res) => {
   try {
     const { imagePath } = req.params;
-    const { newImagePath, newQuestion } = req.body;
+    let { newImagePath, newQuestion } = req.body;
     const imageFile = req.file;
 
     const imageData = await query('SELECT ID, Question FROM Images WHERE FilePath = ?', [imagePath]);
     if (imageData.length === 0) {
-      return res.status(404).json({ message: "Image non trouvée" });
+      return res.redirect(req.originalUrl + `/erreur/404?message=${encodeURIComponent('Image non trouvée')}`);
     }
 
     const imageID = imageData[0].ID;
@@ -171,8 +173,19 @@ router.post('/api/updateimage/:imagePath', authenticateToken, upload.single('ima
     if (!imageFile && newImagePath && newImagePath !== imagePath) {
       const existingImage = await query('SELECT ID FROM Images WHERE FilePath = ?', [newImagePath]);
       if (existingImage.length > 0) {
-        return res.status(400).json({ message: "Le nom de l'image est déjà utilisé" });
+        return res.redirect(req.originalUrl + `/erreur/400?message=${encodeURIComponent('Le nom de l image est déjà utilisé')}`);
       }
+    }
+    //verifications du nom et mise en place .png et .jpg
+    let ext = path.extname(imageFile.originalname).toLowerCase(); // Récupère l'extension de l'image actuelle
+    let newExt = path.extname(newImagePath).toLowerCase(); // Récupère l'extension de la nouvelle image
+    console.log(ext + " : "+ newExt);
+    // Vérifie si l'extension de la nouvelle image est .png ou .jpg
+    if (newExt !== '.png' && newExt !== '.jpg') {
+      newImagePath += ext; // Si l'extension n'est pas .png ou .jpg, ajoute l'extension de l'image actuelle
+    } else if (ext !== newExt) {
+      // Si l'extension de l'image actuelle est différente de celle de la nouvelle image, modifie l'extension de la nouvelle image pour qu'elle corresponde à celle de l'image actuelle
+      newImagePath = path.basename(newImagePath, newExt) + ext;
     }
 
     const oldFolder = oldQuestion ? "singuliers" : "neutres";
@@ -186,7 +199,7 @@ router.post('/api/updateimage/:imagePath', authenticateToken, upload.single('ima
       fs.rename(imageFile.path, newFilePath, function (err) {
         if (err) {
           console.log(err);
-          return res.status(500).json({ message: 'Erreur lors du déplacement du fichier' });
+          return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('Erreur lors du déplacement du fichier')}`);
         }
 
         imageFile.path = newFilePath;
@@ -202,16 +215,14 @@ router.post('/api/updateimage/:imagePath', authenticateToken, upload.single('ima
         fs.renameSync(oldFolderPath, newFolderPath, function(err) {
           if (err) {
             console.log(err)
-            res.status(500).json({ message: 'Erreur lors du déplacement du fichier' });
-            return;
+            return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('Erreur lors du déplacement du fichier')}`);
           }
         });
       }else{
         fs.unlinkSync(oldFolderPath, function(err) {
           if (err) {
             console.log(err)
-            res.status(500).json({ message: 'Erreur lors de la suppression de l\'image précédente' });
-            return;
+            return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('Erreur lors de la suppression de l\'image précédente')}`);
           }
         });
       }
@@ -222,8 +233,7 @@ router.post('/api/updateimage/:imagePath', authenticateToken, upload.single('ima
       fs.renameSync(oldFilePath, newFilePath, function(err) {
         if (err) {
           console.log(err)
-          res.status(500).json({ message: 'Erreur lors du renommage du fichier' });
-          return;
+          return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('Erreur lors du renommage du fichier')}`);
         }
       });
     }
@@ -238,7 +248,7 @@ router.post('/api/updateimage/:imagePath', authenticateToken, upload.single('ima
     res.json({ message: 'Image mise à jour avec succès' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Erreur de serveur' });
+    return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('Erreur de serveur')}`);
   }
 });
 
@@ -302,13 +312,13 @@ function generateAccessToken(userId) {
     const token = req.cookies['authToken'];
   
     if (!token) {
-      return res.status(401).json({ message: 'Jetons d\'authentification manquants' });
+      return res.redirect(req.originalUrl + `/erreur/401?message=${encodeURIComponent('Jetons d\'authentification manquants')}`);
     }
   
     jwt.verify(token, jwtSecret, (error, decoded) => {
       if (error) {
         console.error(error);
-        return res.status(403).json({ message: 'Échec de l\'authentification du jeton' });
+        return res.redirect(req.originalUrl + `/erreur/403?message=${encodeURIComponent('Échec de l\'authentification du jeton')}`);
       }
   
       req.token = token;
@@ -318,28 +328,28 @@ function generateAccessToken(userId) {
       connection.query('SELECT ID FROM Token WHERE UserID = ? AND TokenValue = ? AND Expired > NOW()', [decoded.id, token], (error, results, fields) => {
         if (error) {
           console.error(error);
-          return res.status(500).json({ message: 'Erreur interne' });
+          return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('Erreur interne')}`);
         }
   
         if (results.length === 0) {
           connection.query('SELECT ID FROM Token WHERE UserID = ? AND TokenValue = ? AND Expired < NOW()', [decoded.id, token], (error, results, fields) => {
             if (error) {
               console.error(error);
-              return res.status(500).json({ message: 'Erreur interne' });
+              return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('Erreur interne')}`);
             }
       
             if (results.length >0) {
               connection.query('DELETE FROM Token WHERE UserID = ? AND TokenValue = ?', [decoded.id, token], (error, results, fields) => {
                 if (error) {
                   console.error(error);
-                  return res.status(500).json({ message: 'Erreur interne' });
+                  return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('Erreur interne')}`);
                 }
               });
             }
           });
           // Supprimer le cookie du navigateur
           res.clearCookie('authToken');
-          return res.status(401).json({ message: 'Authentification invalide. Veuillez vous reconnecter.' });
+          return res.redirect(req.originalUrl + `/erreur/401?message=${encodeURIComponent('Authentification invalide. Veuillez vous reconnecter')}`);
         }
         next();
       });
@@ -353,14 +363,14 @@ function generateAccessToken(userId) {
   
       const users = await query('SELECT ID,Password FROM users WHERE Username = ?', [username]);
       if (users.length === 0) {
-        return res.status(401).json({ message: 'Nom d utilisateur invalide'  });
+        return res.redirect(req.originalUrl + `?message=${encodeURIComponent('Nom d utilisateur invalide')}`);
       }
   
       const user = users[0];
       const passwordMatch = await bcrypt.compare(password, user.Password);
   
       if (!passwordMatch) {
-        return res.status(401).json({ message: 'mot de passe invalide' });
+        return res.redirect(req.originalUrl + `?message=${encodeURIComponent('mot de passe invalide')}`);
       }
   
       const tokens = await query('SELECT ID FROM Token WHERE UserID = ?', [user.ID]);
@@ -379,7 +389,7 @@ function generateAccessToken(userId) {
       res.redirect('/accueil');
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Erreur connextion a la BD' });
+      return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('Erreur connextion a la BD')}`);
     }
   });
   
@@ -393,6 +403,7 @@ router.get('/api/user', authenticateToken, async (req, res) => {
   try {
       const user = await query('SELECT * FROM Users WHERE ID = ?', [req.user.id]);
       if (user.length === 0) {
+        
           return res.status(404).json({ message: "Utilisateur non trouvé" });
       }
 
@@ -408,20 +419,30 @@ router.get('/api/capchats', authenticateToken, async (req, res) => {
   try {
       const capchats = await query('SELECT COUNT(Images.ID) as nombreImage, ImageSets.URLUsage FROM ImageSets LEFT JOIN Images ON Images.ImageSetID = ImageSets.ID WHERE ImageSets.UserID = ? GROUP BY ImageSets.ID', [req.user.id]);
       if (capchats.length === 0) {
-          return res.status(404).json({ message: "Aucun CapChat trouvé" });
+        return res.redirect(req.originalUrl + `/erreur/404?message=${encodeURIComponent('Aucun CapChat trouvé')}`);
       }
 
       res.json(capchats);
   } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Erreur de serveur' });
+      return res.redirect(req.originalUrl + `/erreur/500?message=${encodeURIComponent('Erreur de serveur')}`);
   }
 });
 
+router.get('*/erreur/:idErreur', (req, res) => {
+  const idErreur = req.params.idErreur;
+  const message = req.query.message || 'Erreur inconnue';
+
+  // Utilisez l'ID d'erreur pour charger le fichier HTML approprié
+  const filePath = path.join(__dirname, '..', 'views', 'erreur', `${idErreur}.html`);
+
+  // Renvoyer le fichier HTML avec le message d'erreur
+  res.sendFile(filePath, { message });
+});
 
 router.get('*', (req, res) => {
-    res.redirect('/');
-  });
+  res.redirect(req.originalUrl + `/erreur/404?message=${encodeURIComponent('La page existe pas')}`);
+});
   
 
 module.exports = router;
