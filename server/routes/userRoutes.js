4d45e11ab445dcccab397dee1aedc4dedcec31ec
Hugo <78080239+HugoBiegas@users.noreply.github.com>
2023-05-22 13:18:35 +0200

    update

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -38,6 +38,8 @@ router.use('/views/image/singuliers', express.static(path.join(__dirname, '..', 
 router.use('/views/image', express.static(path.join(__dirname, '..', 'views', 'image')));
 router.use('/views/erreur', express.static(path.join(__dirname, '..', 'views', 'erreur')));
 router.use('/node_modules', express.static(path.join(__dirname, '..','..', 'node_modules')));
+router.use('/css', express.static(path.join(__dirname, '..', 'views','css')));
+router.use('/js', express.static(path.join(__dirname, '..', 'views','js')));
 
 
 router.get('/', (req, res) => {
@@ -661,16 +663,9 @@ router.get('/api/allcapChat', async (req, res) => {
 
 router.post('/api/newCapChat', authenticateToken, upload.array('images'), async (req, res) => {
   try {
-    const { capchatName, theme, nouveauTheme, images } = req.body;
+    const { capchatName, theme, nouveauTheme } = req.body;
     const userID = req.user.id;
-    const imageFile = req.file.originalname;
-    
-    console.log('capchatName:', capchatName);
-    console.log('theme:', theme);
-    console.log('nouveauTheme:', nouveauTheme);
-    console.log('images:', images);
-    console.log('userID:', userID);
-    console.log('imageFile:', imageFile);
+    const imagesData = JSON.parse(req.body.imagesData);
 
     // Vérifier si le thème est nouveau
     let themeID;
@@ -684,18 +679,14 @@ router.post('/api/newCapChat', authenticateToken, upload.array('images'), async 
       themeID = theme;
     }
 
-    console.log('themeID:', themeID);
-
     // Créer le capChat
     const insertCapChatQuery = 'INSERT INTO ImageSets (UserID, ThemeID, URLUsage) VALUES (?, ?, ?)';
     const capChatResult = await query(insertCapChatQuery, [userID, themeID, capchatName]);
     const imageSetID = capChatResult.insertId;
 
-    console.log('imageSetID:', imageSetID);
-
-    // Parcourir les images et les ajouter à la base de données
-    for (const image of images) {
-      const { name, question } = image;
+    // Parcourir les données des images et les enregistrer dans la base de données
+    for (let i = 0; i < imagesData.length; i++) {
+      const { name, question } = imagesData[i];
 
       // Vérifier si l'image a une question
       const isQuestionImage = !!question;
@@ -703,25 +694,22 @@ router.post('/api/newCapChat', authenticateToken, upload.array('images'), async 
       // Déterminer le dossier de destination en fonction de la présence d'une question
       const destinationFolder = isQuestionImage ? 'singuliers' : 'neutres';
 
-      // Vérifier l'extension du fichier
-      let newFileName = name;
-      const extension = path.extname(name);
-      if (!extension || (extension !== '.png' && extension !== '.jpg' && extension !== '.jpeg')) {
-        // Obtenir l'extension de l'image originale
-        const originalExtension = path.extname(imageFile);
-  
-        // Vérifier l'extension de l'image originale
-        if (originalExtension === '.png' || originalExtension === '.jpg' || originalExtension === '.jpeg') {
-          newFileName += originalExtension;
-        } else {
-          return res.status(400).json({ error: 'Le fichier doit être une image PNG ou JPEG' });
-        }
-      }
+    // Vérifier l'extension du fichier
+    let newFileName = name;
+    const extension = path.extname(newFileName);
+    if (!extension || (extension !== '.png' && extension !== '.jpg' && extension !== '.jpeg')) {
+      const originalExtension = path.extname(imageFile.originalname);
+    if (originalExtension === '.png' || originalExtension === '.jpg' || originalExtension === '.jpeg') {
+      newFileName = newFileName + originalExtension;
+    } else {
+      return res.status(400).json({ error: 'Le fichier doit être une image PNG ou JPEG' });
+    }
+  }
 
-      console.log('newFileName:', newFileName);
+
 
       // Déplacer le fichier vers le dossier de destination
-      const sourcePath = path.join(__dirname, '..', 'views', 'image', imageFile);
+      const sourcePath = path.join(__dirname, '..', 'views', 'image', name);
       const destinationPath = path.join(__dirname, '..', 'views', 'image', destinationFolder, newFileName);
       fs.renameSync(sourcePath, destinationPath);
 
@@ -736,6 +724,8 @@ router.post('/api/newCapChat', authenticateToken, upload.array('images'), async 
     res.status(500).json({ error: 'Erreur de serveur' });
   }
 });
+
+
 
 
 
